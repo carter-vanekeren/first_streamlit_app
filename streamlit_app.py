6a63c09ceb14e73bcd8f6366c52473fb22f71342
Carter Van Ekeren <vanek015@umn.edu>
2022-05-25 16:18:21 -0500

    Update streamlit_app.py
    Adding emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 and blueberry oatmeal")
-streamlit.text("Kale spinach and rocket smoothie")
-streamlit.text("Hardboiled free range egg")
-
+streamlit.text("🥣 Omega 3 and blueberry oatmeal")
+streamlit.text("🥗 Kale spinach and rocket smoothie")
+streamlit.text("🐔 Hardboiled free range egg")
+streamlit.text("🥑🍞 Hardboiled free range egg")
